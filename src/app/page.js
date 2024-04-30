9eab0a797e758d333ebe70569a58e154855f9b6f
Philippos Georgiou <philipposgeorgiou7@gmail.com>
2024-04-30 17:02:22 +0100

    update globals.css

--- src/app/page.js
+++ src/app/page.js
@@ -9,24 +9,21 @@ export default function Home() {
             <header></header>
             <main className={styles["flex_container_col"]}>
                 <h1>Welcome to my portfolio website!</h1>
-                <article>
-                    <section id="about-me">
-                        <h2>About Me</h2>
-                        <AboutMe />
-                    </section>
-                </article>
-                <article>
-                    <section id="projects">
-                        <h2>Projects</h2>
-                        <Projects />
-                    </section>
-                </article>
-                <article>
-                    <section id="contact">
-                        <h2>Contact</h2>
-                        <Contact />
-                    </section>
-                </article>
+
+                <section id="about-me">
+                    <h2>About Me</h2>
+                    <AboutMe />
+                </section>
+                <section id="projects">
+                    <h2>Projects</h2>
+                    <Projects />
+                </section>
+                <section id="contact">
+                    <h2>Contact</h2>
+                    <Contact />
+                </section>
+
+
             </main>
         </>
     );
