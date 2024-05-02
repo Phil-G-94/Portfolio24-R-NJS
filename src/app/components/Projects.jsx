762e14063d9f9dbb63af06d3767cccdc14607a83
Philippos Georgiou <philipposgeorgiou7@gmail.com>
2024-05-02 17:53:58 +0100

    add link to Projects cmp

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,14 +1,43 @@
 import styles from "../page.module.css";
+import projects from "../data/projects";
+import Link from "next/link";
 
 export default function Projects() {
     return (
-        <article className={styles["flex_container_col"]}>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-                dolore illum animi illo odit consectetur mollitia soluta eveniet
-                ipsa aliquam ut autem et modi libero repellendus distinctio qui,
-                consequatur a.
-            </p>
+        <article>
+            <section>
+                <div className={styles["project_flex_container_col"]}>
+                    {projects.map((project) => {
+                        return (
+                            <Link
+                                key={project.id}
+                                href="/"
+                                className={styles["project_info_link"]}
+                            >
+                                <div className={styles["project_info_grid"]}>
+                                    <p className={styles["project_info_title"]}>
+                                        {project.title}
+                                    </p>
+                                    <p
+                                        className={
+                                            styles["project_info_description"]
+                                        }
+                                    >
+                                        {project.description}
+                                    </p>
+                                    <img
+                                        className={styles["project_info_icon"]}
+                                        src="svg/box-arrow-up-right.svg"
+                                        alt="icon"
+                                        width="32"
+                                        height="32"
+                                    />
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </div>
+            </section>
         </article>
     );
 }
