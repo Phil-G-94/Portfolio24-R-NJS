651099da89c992beb78c7976a0fdc514b13ee09b
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-06-23 21:48:51 +0100

    add error and not-found UI

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>404: Page not found!</h2>
+
+            <p> SVG here! </p>
+        </div>
+    );
+}
