121c8a825fd2c2903e246b994a549716aea20050
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-06-24 23:20:41 +0100

    add page not found sprite

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,22 @@
 "use client";
 
+import styles from "./page.module.css";
+
+import Link from "next/link";
+
 export default function NotFound() {
     return (
-        <div>
+        <div className={styles["grid_container"]}>
             <h2>404: Page not found!</h2>
 
-            <p> SVG here! </p>
+            <p>
+                The page you are looking for does not exist. Check the URL or
+                head <Link href="../">back</Link>
+            </p>
+
+            <svg style={{ fill: "white" }}>
+                <use href="/img/errorSprites.svg#page-not-found"></use>
+            </svg>
         </div>
     );
 }
