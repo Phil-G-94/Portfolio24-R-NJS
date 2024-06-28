7740802e53b69e569958ae8070e636ca730d698d
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-06-28 21:54:46 +0100

    update nextjs; add generic error sprite and styling

--- src/app/error.js
+++ src/app/error.js
@@ -1,16 +1,30 @@
 "use client";
 
+import Link from "next/link";
+import styles from "./page.module.css";
+
 import { useEffect } from "react";
 
 export default function Error({ error, reset }) {
     useEffect(() => {
-        console.error(error);
+        console.log(error);
     }, [error]);
 
     return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button onClick={() => reset()}>Try again</button>
+        <div className={styles["grid_container"]}>
+            <h2>Error!</h2>
+
+            <p>Something isn&apos;t quite right. </p>
+
+            <svg style={{ fill: "white" }}>
+                <use href="/img/errorSprites.svg#app-error"></use>
+            </svg>
+            <div>
+                <button onClick={() => reset()}>Try Again</button>
+                <button>
+                    <Link href="/">Go Home</Link>
+                </button>
+            </div>
         </div>
     );
 }
