80ac2a64eb22b4f79904c7d06f120aa80c6dc7a8
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-03-14 16:02:52 +0000

    fix #7: update Projects cmp to import json file

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,20 +1,13 @@
+import projects from "../data/projects.json";
 import ProjectsCarousel from "./ProjectsCarousel";
 
 export default async function Projects() {
-    const baseUrl = process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000";
-
-    const res = await fetch(`${baseUrl}/data/projects.json`);
-
-    const data = await res.json();
-
     return (
         <>
             <h2 className="text-center">Projects</h2>
             <section className="flex flex-col justify-center">
                 <article>
-                    <ProjectsCarousel projects={data} />
+                    <ProjectsCarousel projects={projects} />
                 </article>
             </section>
         </>
