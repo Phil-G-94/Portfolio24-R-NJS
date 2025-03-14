c2fc1ddae16fd5a99a2689a001482833db1f602c
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-03-14 15:43:05 +0000

    fix #5: revert back to reading via fs

--- src/app/projects/[projectId]/page.js
+++ src/app/projects/[projectId]/page.js
@@ -1,13 +1,45 @@
+// import ProjectDetail from "@/app/components/ProjectDetail";
+
+// export async function generateStaticParams() {
+//     const baseUrl = process.env.VERCEL_URL
+//         ? `https://${process.env.VERCEL_URL}`
+//         : "http://localhost:3000";
+
+//     const res = await fetch(`${baseUrl}/data/projects.json`);
+
+//     const data = await res.json();
+
+//     const params = data.map((project) => ({ id: project.id.toString() }));
+
+//     return params.map((param) => ({ params: param }));
+// }
+
+// export default async function Page({ params }) {
+//     const { projectId } = await params;
+
+//     const baseUrl = process.env.VERCEL_URL
+//         ? `https://${process.env.VERCEL_URL}`
+//         : "http://localhost:3000";
+
+//     const res = await fetch(`${baseUrl}/data/projects.json`);
+
+//     const data = await res.json();
+
+//     console.log(data);
+
+//     const project = data.find((p) => p.id.toString() === projectId);
+
+//     return <ProjectDetail project={project} />;
+// }
+
 import ProjectDetail from "@/app/components/ProjectDetail";
 
 export async function generateStaticParams() {
-    const baseUrl = process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000";
+    const filePath = path.resolve(process.cwd(), "src/app/data/projects.json");
 
-    const res = await fetch(`${baseUrl}/data/projects.json`);
+    const file = await fs.readFile(filePath, "utf-8");
 
-    const data = await res.json();
+    const data = JSON.parse(file);
 
     const params = data.map((project) => ({ id: project.id.toString() }));
 
@@ -17,13 +49,11 @@ export async function generateStaticParams() {
 export default async function Page({ params }) {
     const { projectId } = await params;
 
-    const baseUrl = process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000";
+    const filePath = path.resolve(process.cwd(), "src/app/data/projects.json");
 
-    const res = await fetch(`${baseUrl}/data/projects.json`);
+    const file = await fs.readFile(filePath, "utf-8");
 
-    const data = await res.json();
+    const data = JSON.parse(file);
 
     const project = data.find((p) => p.id.toString() === projectId);
 
