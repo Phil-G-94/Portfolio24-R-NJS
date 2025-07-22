1207a491b8fcd3a786944c5f61eb36d869bbc5d9
Phil Georgiou <philipposgeorgiou7@gmail.com>
2025-07-22 11:25:17 +0100

    Fix: Error page improvements

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,22 @@
 "use client";
 
 import Link from "next/link";
+import { HomeIcon } from "@heroicons/react/24/solid";
 import "./globals.css";
 
 export default function NotFound() {
     return (
         <div className="flex flex-col justify-center items-center gap-16">
-            <h2 className="mt-12">404: Page not found!</h2>
+            <h2 className="mt-12 text-xl">404: Page not found!</h2>
 
-            <p>
-                The page you are looking for does not exist. Check the URL or
-                head <Link href="../">back</Link>
-            </p>
+            <p>You&apos;ve hit a route that doesn&apos;t exist...</p>
+
+            <div className="flex gap-2">
+                <p className="self-end">Check the URL or head back</p>
+                <Link href="../">
+                    <HomeIcon className="size-10 hover:scale-110 hover:drop-shadow-lg" />
+                </Link>
+            </div>
         </div>
     );
 }
